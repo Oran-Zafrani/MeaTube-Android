62d00bec583d14f3e0b660a20095e4e7d05d4558
BarShwarts1 <bar.shwarts@live.biu.ac.il>
2024-05-25 20:03:41 +0300

    signin with start signup

--- app/src/main/java/com/example/footube/SignIn.java
+++ app/src/main/java/com/example/footube/SignIn.java
@@ -1,14 +1,29 @@
 package com.example.footube;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class SignIn extends AppCompatActivity {
-
+    TextView linkToSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        Intent intent = new Intent(this, SignUp.class);
+        linkToSignUp = findViewById(R.id.tvSignUpLink);
+
+        linkToSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
