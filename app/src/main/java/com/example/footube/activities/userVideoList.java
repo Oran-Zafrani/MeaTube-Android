afe88becec775e3e3c26f694c83871e249a1fdd7
BarShwarts1 <bar.shwarts@live.biu.ac.il>
2024-08-24 17:02:23 +0300

    add activity of user video list and first disign

---
+++ app/src/main/java/com/example/footube/activities/userVideoList.java
@@ -0,0 +1,22 @@
+package com.example.footube.activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.footube.R;
+
+public class userVideoList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_user_video_list);
+        
+    }
+}
